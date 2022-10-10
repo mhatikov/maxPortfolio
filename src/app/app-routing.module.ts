9ca7d75f807744d19a5e19c1d342a3a984f543a4
Mihail Kramskov <kramskov.m@yandex.ru>
2022-10-10 21:56:21 +0400

    feat 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CvComponent } from './components/cv/cv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:CvComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
