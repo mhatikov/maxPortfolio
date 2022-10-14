95dd84840578c84cce79827575da974e0bc2a73e
Mihail Kramskov <kramskov.m@yandex.ru>
2022-10-14 10:35:13 +0400

    v.4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CvComponent } from './components/cv/cv.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', component:CvComponent}
+  {path: '', component: AppComponent}
 ];
 
 @NgModule({
